48ff7b8fd6e4f502fc046f841f228ea74ab124e6
Camille <camille.morfin@etu.univ-lyon1.fr>
2024-08-30 18:47:22 +0200

    add calendrier

--- js/calendar.js
+++ js/calendar.js
@@ -16,8 +16,14 @@ document.addEventListener('DOMContentLoaded', function() {
             day: 'Jour'
         },
         events: [
-            // Vos événements ici
-        ]
+
+        ],
+        eventDidMount: function(info) {
+            const eventType = info.event.extendedProps.type;
+            if (eventType) {
+                info.el.classList.add(eventType);
+            }
+        }
     });
 
     calendar.render();
@@ -38,4 +44,48 @@ document.addEventListener('DOMContentLoaded', function() {
     window.addEventListener('resize', function() {
         updateHeaderToolbar();
     });
+
+    const form_activity = document.getElementById('activity-form');
+
+    form_activity.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const name = document.getElementById('activity-name').value;
+        const type = document.getElementById('activity-type').value;
+        const date = document.getElementById('activity-date').value;
+        const startTime = document.getElementById('activity-start-time').value;
+        const endTime = document.getElementById('activity-end-time').value;
+        const location = document.getElementById('activity-location').value;
+        const description = document.getElementById('activity-description').value;
+        const repeat = document.getElementById('activity-repeat').checked;
+
+        const newEvent = {
+            title: name,
+            start: `${date}T${startTime}`,
+            end: `${date}T${endTime}`,
+            description: description,
+            location: location,
+            extendedProps: {
+                type: type
+            }
+        }
+
+        calendar.addEvent(newEvent);
+
+        const modal = bootstrap.Modal.getInstance(document.getElementById('activityModal'));
+        modal.hide();
+    });
+
+    /* afficahge légende */
+        const legendToggle = document.getElementById('legend-toggle');
+        const eventLegend = document.getElementById('event-legend');
+
+        legendToggle.addEventListener('click', function() {
+            // Alterne la visibilité de la légende
+            if (eventLegend.style.display === 'none' || eventLegend.style.display === '') {
+                eventLegend.style.display = 'block';
+            } else {
+                eventLegend.style.display = 'none';
+            }
+        });
 });
